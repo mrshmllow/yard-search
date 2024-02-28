ec22c82f0624ec91701374ceec1f81eaca58a7f8
marshmallow <marshycity@gmail.com>
2024-02-28 20:39:23 +1100

    feb 28th progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,49 @@
-export default async function Home({
-  params,
-  searchParams,
-}: {
-  params: { slug: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}) {
-  const query = searchParams["query"] !== undefined && !Array.isArray(searchParams["query"]) ? searchParams["query"] : "";
-  const data = {
-  };
+"use client";
 
-  return (
-    <main className="">
-      <h1>yard-search</h1>
+import { InstantSearch, SearchBox, Hits, Highlight } from 'react-instantsearch';
+import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
 
-      <form>
-        <input name="query" defaultValue={query}></input>
-        <button type="submit">Search</button>
-      </form>
-    </main>
-  );
+const { searchClient } = instantMeiliSearch(
+	'http://127.0.0.1:7700',
+	'HWoz-31otPLUyXZmEfFDWpC3osm3XTW0Ebv3GTj5yrg',
+	{
+		meiliSearchParams: {
+			attributesToRetrieve: [
+				"id",
+				"chapter",
+				"youtube_id",
+				"uploaded"
+			],
+			attributesToCrop: ["trans"],
+			cropLength: 30
+		},
+	}
+);
+
+const Hit = ({ hit }) => <div className="border border-black rounded flex flex-col p-4">
+	<Highlight attribute="chapter" className="font-bold" hit={hit} />
+	<Highlight attribute="trans" hit={hit} />
+</div>;
+
+export default function Home() {
+	return (
+		<main className="p-4">
+			<h1>yard-search</h1>
+
+			<InstantSearch
+				indexName="chapters"
+				searchClient={searchClient}
+			>
+				<SearchBox
+					classNames={{
+						form: "flex",
+						input: "flex-grow",
+					}}
+				/>
+				<Hits hitComponent={Hit} classNames={{
+					list: "flex flex-col gap-4"
+				}} />
+			</InstantSearch>
+		</main>
+	);
 }
