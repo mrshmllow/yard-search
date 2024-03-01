1a3338bb6abe5d86a43d3bf6e8158a6ee717a159
marshmallow <marshycity@gmail.com>
2024-03-02 09:24:34 +1100

    way better transcript view

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import SortBy from './(components)/SortBy';
 import { Hit, BaseHit } from "instantsearch.js";
 import { env } from "@/env";
 
-
 const { searchClient } = instantMeiliSearch(
 	env.NEXT_PUBLIC_MEILISEARCH_URL,
 	env.NEXT_PUBLIC_MEILISEARCH_KEY,
@@ -35,6 +34,19 @@ interface Chapter {
 	episode: number;
 }
 
+const FULL_TIMESTAM_PATTERN = /(?<hours>\d{2}):(?<minutes>\d{2}):(?<seconds>\d{2})\.\d{3} --&gt; \d{2}:\d{2}:\d{2}\.\d{3}/;
+const BROKEN_TIMESTAMP_PATTERN = /\d+:?\.?|--&gt;/g;
+const EMPTY_ELLIPSIS = /^ *?…$/;
+
+function strip_timestamps(string: string) {
+	// Must do this to maintain html codes
+	const decodedString = string.replace(/&#(\d+);/g, (match, decimal) => {
+		return String.fromCharCode(parseInt(decimal, 10));
+	});
+
+	return decodedString.replace(FULL_TIMESTAM_PATTERN, "").replaceAll(BROKEN_TIMESTAMP_PATTERN, "")
+}
+
 const Hit = ({ hit }: { hit: Hit<BaseHit & Chapter> }) => <div className="border border-black rounded flex flex-col">
 	<div className="rounded-lg border-gray-500 border px-2 py-2 flex flex-col">
 		<div className="flex place-items-center gap-2">
@@ -45,9 +57,29 @@ const Hit = ({ hit }: { hit: Hit<BaseHit & Chapter> }) => <div className="border
 			}} />
 		</div>
 
-		<Highlight attribute="trans" hit={hit} classNames={{
-			highlighted: "bg-white text-black",
-		}} />
+		<div className="flex flex-col gap-2">
+			{hit._highlightResult.trans.value.split("\n\n").filter(line => !line.startsWith("WEBVTT")).map(string => {
+				const stripped = strip_timestamps(string);
+
+				console.log(stripped)
+
+				if (stripped.match(EMPTY_ELLIPSIS)) return <></>
+
+				return <>
+					<Highlight attribute="split" hit={{
+						"_highlightResult": {
+							"split": {
+								"value": stripped
+							}
+						}
+					}}
+						classNames={{
+							highlighted: "bg-white text-black rounded-sm",
+						}}
+					/>
+				</>
+			})}
+		</div>
 	</div>
 </div>;
 
